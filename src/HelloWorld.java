4f28596334bf483f5c8367856cad8d59cac699cc
desmondpratt <dcpratt002@gmail.com>
2022-08-30 14:56:22 -0400

    Update file to include today's changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,11 @@
 //By convention, class names are capitalized
 public class HelloWorld {
-    public static void main(String [] args){
+    public static void main(String [] args) {
         System.out.println("Hello World");
+
+        int x = 5;
+        boolean z = true;
+        double pi = 3.14159;
     }
 
 }
