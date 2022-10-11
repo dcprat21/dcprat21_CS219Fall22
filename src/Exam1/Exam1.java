e983eb5d72fb30012ca27443abd618e1e76dd86f
desmondpratt <dcpratt002@gmail.com>
2022-10-11 14:36:38 -0400

    Exam1, edited

--- src/Exam1/Exam1.java
+++ src/Exam1/Exam1.java
@@ -2,46 +2,48 @@ package Exam1;
 
 import java.util.Scanner;
 
-public class Exam1{
-    public static void main(String [] args){
-        Scanner kbd = new Scanner(System.in);
-        int n = getNumber(kbd);
-        System.out.println("Your number is..." + n);
+public class Exam1 {
+    public static void main(String[] args) {
 
+        // scanner is used, so user can import inputs
+        Scanner s = new Scanner(System.in);
 
-    }
+        System.out.print("Please enter a string of numbers: ");
 
-    public static int getNumber(Scanner s){
         boolean valid = false;
         int getNumber = 0;
 
-        while ( !valid ){
-            System.out.println("Please enter a string of numbers: ");
-            if (s.hasNextInt()){
+        int n = 0;
+        while (!valid) {
+            System.out.print("Please enter a string of numbers: ");
+            if (s.hasNextInt()) {
                 getNumber = s.nextInt();
-                if(getNumber < 0){
+                if (getNumber < 0) {
                     System.out.printf("Error: number of years cannot be negative. You entered " + getNumber);
                     System.out.println("");
-                }
-                else{
+                } else {
                     valid = true;
                 }
+                while (getNumber > 0) {
+                    int check = 0;
+                    check = getNumber % 10;
+                    getNumber = getNumber / 10;
 
-            }
-            else{
+                    if (check > 2 && check < 7) {
+                        n = n + 1;
+                    }
+
+
+                }
+
+            } else {
                 String word = s.next();
                 System.out.println("Your input is invalid because it is not a numeral value. You said: " + word);
-
-
-
             }
 
 
         }
-        return getNumber;
 
-
-        }
-
-
+        System.out.println("Your number is..." + n);
+    }
 }
